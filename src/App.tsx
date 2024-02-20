d726266929e8e2fb1ab1e4d4c455ef3f7e484d7f
Bruno Gabado <brunogabado2hotmail.com>
2024-02-20 23:21:59 +0000

    start of the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <>
+    
+    <div className="wave-edge-div"><h1>Here we go</h1></div>
+    <div><h1>Here we go</h1></div>
+    </>
+  )
+}
+
+export default App
