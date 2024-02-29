78d7d8c2893831db8a4b2317b8f2811a9b077012
Bruno Gabado <brunogabado2hotmail.com>
2024-02-29 20:05:02 +0000

    Responsive carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { projectProps } from "../sections/ProjectsSection";
-// import NextSvg from "../svg's/NextSvg";
-// import PrevSvg from "../svg's/PrevSvg";
 import WebSvg from "../svg's/WebSvg";
 import GithubIcon from "../svg's/GithubIconSvg";
 
@@ -10,17 +8,18 @@ const Carousel = ({ projects }: { projects: projectProps[] }) => {
   const [buttonsOpen, setButtonsOpen] = useState<boolean>(false);
 
   const nextSlide = () => {
+    setButtonsOpen(false);
     let newIndex: number = index + 1 > projects.length - 1 ? 0 : index + 1;
     setIndex(newIndex);
   };
 
   const prevSlide = () => {
+    setButtonsOpen(false);
     let newIndex: number = index - 1 < 0 ? projects.length - 1 : index - 1;
     setIndex(newIndex);
   };
 
   const handleScroll = (event: React.WheelEvent<HTMLDivElement>) => {
-    setButtonsOpen(false);
     if (event.deltaY > 0) {
       // Scrolling down
       nextSlide();
@@ -31,31 +30,43 @@ const Carousel = ({ projects }: { projects: projectProps[] }) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center text-center mt-[70px] pb-12 text-lg w-[600px]" onWheel={handleScroll}>
-      <div className="flex justify-center border-2 bg-white w-[430px] h-[200px] opacity-25 transition-opacity duration-300" onClick={prevSlide}>
+    <div className="flex flex-col xl:flex-row overscroll-contain justify-center items-center text-center my-[70px] pb-12 text-lg" onWheel={handleScroll}>
+      <div
+        className="flex justify-center absolute lg:left-[100px] rounded-lg border-2 bg-white w-[180px] h-[90px] opacity-25 transition-opacity duration-300 min-[450px]:w-[220px] min-[450px]:h-[110px] md:h-[150px] md:w-[300px] lg:h-[180px] lg:w-[360px] xl:h-[210px] xl:w-[420px]"
+        onClick={prevSlide}
+      >
         <img src={`${projects[index - 1 < 0 ? projects.length - 1 : index - 1].image}.png`} className="h-full object-cover" />
       </div>
 
-      <div className="flex z-10 gap-2">
+      <div className="flex flex-col z-10 transition delay-200 duration-300 ease-in-out hover:scale-[1.1]">
         <div
-          className="flex justify-center transition delay-150 duration-300 ease-in-out border-2 bg-white w-[580px] h-[270px] hover:scale-[1.05] transition-opacity duration-300"
+          className="flex justify-center transition rounded-lg delay-150 duration-300 ease-in-out border-2 b bg-white w-[270px] h-[130px]  min-[450px]:w-[330px] min-[450px]:h-[160px] sm:h-[200px] sm:w-[420px] md:h-[250px] md:w-[530px] lg:h-[300px] lg:w-[640px] xl:h-[400px] xl:w-[850px] "
           onClick={() => setButtonsOpen(!buttonsOpen)}
         >
           <img src={`${projects[index].image}.png`} className="h-full object-cover" />
         </div>
         {buttonsOpen && (
-          <div className="flex flex-col justify-center flex justify-around items-center">
-            <a className="flex text-white items-center" href={projects[index].liveLink}>
+          <div className="flex justify-center flex rounded-lg justify-around items-center border-2 h-12 bg-black">
+            <a
+              className="flex text-white font-anta items-center transition delay-150 duration-300 ease-in-out hover:scale-125"
+              href={projects[index].liveLink}
+            >
               <WebSvg /> Live App
             </a>
-            <a className="flex text-white items-center" href={projects[index].githubLink}>
+            <a
+              className="flex text-white font-anta items-center transition delay-150 duration-300 ease-in-out hover:scale-125"
+              href={projects[index].githubLink}
+            >
               <GithubIcon /> Repository
             </a>
           </div>
         )}
       </div>
 
-      <div className="flex justify-center border-2 bg-white w-[430px] h-[200px]  opacity-25 transition-opacity duration-300" onClick={nextSlide}>
+      <div
+        className="flex justify-center lg:absolute right-[100px] rounded-lg border-2 bg-white  w-[180px] h-[90px] min-[450px]:w-[220px] min-[450px]:h-[110px] md:h-[150px] md:w-[300px] lg:h-[180px] lg:w-[360px] opacity-25 transition-opacity duration-300  xl:h-[210px] xl:w-[420px]"
+        onClick={nextSlide}
+      >
         <img src={`${projects[index + 1 > projects.length - 1 ? 0 : index + 1].image}.png`} className="h-full object-cover" />
       </div>
     </div>
